f1d61d8d89fc643b9152cac6a453f85234256529
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-06-12 03:21:05 -0700

    10 questions comleted JS quiz 3

---
+++ JS_Quiz3/app.js
@@ -0,0 +1,103 @@
+// JavaScript Quiz 3                                    June 08 , 24
+
+
+// 11 : 
+
+
+
+// 10 : What will be the output of the following JavaScript code 
+/*
+var myarray = ['foo', 'bar', 'baz'];
+myarray.length = 0;
+myarray.push('bin');
+console.log(myarray);
+*/
+// a. [bin, foo, bar, baz]             b. [foo, bar, baz]
+// c. [bin] *                          d. [foo, bar, baz, bin]
+
+
+
+// 09 : Which of these is a valid variable name in JavaScript
+
+// a. ABC 124       b. 12Ab         c. abc123        d. abcGf *
+
+
+
+// 08 : What will be the following code
+
+// return Boolean(10 > 9);
+
+// a. false            b. true *            c. Nan
+
+
+
+// 07 : The lifttime of the parameters begin 
+
+// a. when the sescution of the function is over *     b. None of the above
+// c. when the function is invoked        d. when the function is declined
+
+
+
+// 06 : What does the isNaN function do in JavaScript
+
+// a. None of the above    b. Return false if the argument is not a number
+// c. Return true if the argument is not a number *
+// d. Return ture if the argument is a number
+
+
+
+// 05 : What will be the output of the following JavaScript code
+/*
+var a = 10;
+
+do {
+    a += 1;
+    console.log(a);
+}
+while(a < 5);
+*/
+// a. 1112       b. 11121314         c. 11 *          d. 12345
+
+
+
+// 04 : How to write an IF statement for executing some code if 'i' is NOT
+//      equal to 5
+
+// a. if(i <> 5)    b. if i =! 5 then   c. if i <> 5   d. if(i != 5) *
+
+
+
+// 03 : What is the output of the following JavaScript
+/*
+function x() {
+    document.write(2 + 5 + '8');
+}
+
+x();
+*/
+// a. 258        b. 7            c. 78 *           d. Error
+
+
+
+// 02 : What will be the output of the following JavaScript code
+/*
+function hello() {
+    var a = 10;
+
+    if(a > 5) {
+        a = 7;
+    }
+
+    alert(a);
+}
+
+hello();
+*/
+// a. 10          b. 710          c. 7 *          d. 5
+
+
+
+// 01 : What is the purpose of the legacy DOM
+
+// a. Modify the nodes            b. Making the script modular
+// c. Make the scripting easier   d. Allows access to few keys and elements *
